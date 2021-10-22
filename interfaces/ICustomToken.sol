ba42054aa35a737920800d5f2422c1d18b375b70
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2021-10-22 10:16:34 +0900

    uses arb test mint token as example for updated contracts

---
+++ interfaces/ICustomToken.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MPL-2.0
+pragma solidity ^0.8.0;
+
+// taken from https://github.com/OffchainLabs/arbitrum/blob/001bdeecdefbc4eda9a824ef7b39452b46faeb86/packages/arb-bridge-peripherals/contracts/tokenbridge/ethereum/ICustomToken.sol
+
+interface ArbitrumEnabledToken {
+    /// @notice should return `0xa4b1` if token is enabled for arbitrum gateways
+    function isArbitrumEnabled() external view returns (uint16);
+}
+
+/**
+ * @title Minimum expected interface for L1 custom token (see TestCustomTokenL1.sol for an example implementation)
+ */
+interface ICustomToken is ArbitrumEnabledToken {
+    /**
+     * @notice Should make an external call to EthERC20Bridge.registerCustomL2Token
+     */
+    function registerTokenOnL2(
+        address l2CustomTokenAddress,
+        uint256 maxSubmissionCost,
+        uint256 maxGas,
+        uint256 gasPriceBid,
+        address creditBackAddress
+    ) external;
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    function balanceOf(address account) external view returns (uint256);
+}
+
+interface L1MintableToken is ICustomToken {
+    function bridgeMint(address account, uint256 amount) external;
+}
